20ecfc52516dceef8b9534956b26253b197e4359
burykac <burykac@gmail.com>
2023-02-03 19:36:39 +0100

    Ostatni commit

--- Analiza_danych_Titanic.py
+++ Analiza_danych_Titanic.py
@@ -2,52 +2,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 train_file = pd.read_csv(r".\train.csv")
-train_file = train_file.drop(["SibSp", "Ticket", "Parch", "Cabin", "Embarked"], axis=1)
+train_file = train_file.drop(["PassengerId", "Name", "SibSp", "Ticket", "Parch", "Embarked"], axis=1)
 sns.set_style("darkgrid")
 def menu():
-    decyzja = str(input("Menu programu do analizy danych katastrofy Titanica. Aby wyświetlić interesujące Cię dane \n"
-                        "wybierz odpowiednią literę. Aby poznać dane dotyczące zgonów wybierz A. "))
     while True:
-        if decyzja == "A" or decyzja == "a":
-            jakie_dane1 = str(input("A - ile osób zginęło, a ile przeżyło. \n"
-                                    "B - relacja między klasą podróżniczą, a przeżywalnością. \n"
-                                    "C - relacja między wiekiem, a przeżywalnością. \n"
-                                    "D - relacja między płcią, a przeżywalnością. \n"
-                                    "E - histogram wieku ofiar. \n"
-                                    "F - relacja między opłatą pasażerską, a przeżywalnością. \n"
-                                    "Q - quit").lower())
-            match jakie_dane1:
-                case "a":
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
-                    sns.catplot(data=train_file, x="Survived", kind="count")
-                    plt.show()
-                case "b":
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
-                    sns.catplot(data=train_file, x="Pclass",  kind="count", hue="Survived")
-                    plt.show()
-                case "c":
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
-                    sns.distplot(train_file[train_file["Survived"]=="No"]["Age"], bins=20)
-                    sns.distplot(train_file[train_file["Survived"]=="Yes"]["Age"], bins=20)
-                    # legenda ktory kolor ktory
-                    plt.show()
-                case "d":
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
-                    sns.catplot(data=train_file, x="Sex", hue="Survived", kind="count")
-                    plt.show()
-                case "e":
-                    train_file["Age"].plot.hist(bins=20)
-                    plt.show()
-                case "f":
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
-                    train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
-                    sns.distplot(train_file[train_file["Survived"]=="No"]["Fare"], bins=20)
-                    sns.distplot(train_file[train_file["Survived"]=="Yes"]["Fare"], bins=20)
-                    plt.show()
-                case "q":
-                    break
+        jakie_dane1 = str(input("A - Wyświetl dane w postaci tabeli. \n"
+                                "B - ile osób zginęło, a ile przeżyło. \n"
+                                "C - relacja między klasą podróżniczą, a przeżywalnością. \n"
+                                "D - relacja między wiekiem, a przeżywalnością. \n"
+                                "E - relacja między płcią, a przeżywalnością. \n"
+                                "F - histogram wieku ofiar. \n"
+                                "Q - quit").lower())
+        match jakie_dane1:
+            case "a":
+                print(train_file.head(891))
+            case "b":
+                df = train_file["Survived"].sum()
+                train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+                train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+                sns.catplot(data=train_file, x="Survived", kind="count")
+                plt.show()
+                print(df)
+            case "c":
+                train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+                train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+                sns.catplot(data=train_file, x="Pclass",  kind="count", hue="Survived")
+                plt.show()
+            case "d":
+                train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+                train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+                sns.relplot(data=train_file, x="Fare", y="Age", hue="Survived")
+                plt.show()
+            case "e":
+                train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+                train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+                sns.catplot(data=train_file, x="Sex", hue="Survived", kind="count")
+                plt.show()
+            case "f":
+                train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+                train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+                sns.kdeplot(data=train_file, x="Age", hue="Survived")
+                plt.show()
+            case "q":
+                break
 menu()
--- README.md
+++ README.md
@@ -1,5 +1,59 @@
 # Analiza_danych_dotyczacych_ofiar_Titanica
 
-jak zaimplementować klasy? - program bedzie umozliwial wywietlenie danych zgodnie z zyczeniem uzytkownika w zaleznosci od 
-typu wykresu lub wyswietlanych danych lub zaleznosci
+Analizę danych ofiar Titanica zacząłem od pozbycia się z pliku informacji niekoniecznie przydatnych do znalezienia
+odpowiedzi na postawione pytanie: Co mogło mieć wpływ na przeżycie pasażerów? 
+W celu odpowiedzenia na to pytanie należy najpierw zadać pytanie: Ile osób przeżyło, a ile zginęło? 
+
+df = train_file["Survived"].sum()
+train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+sns.catplot(data=train_file, x="Survived", kind="count")
+plt.show()
+print(df)
+
+Jak widać na zwracanym przez powyższy kod wykresie z 891 zarejestrowanych osób przeżyło ich 342. 
+
+Czy występowała zatem zależność pod względem klasy biletu zakupionego przez pasażera,a przeżywalnością?
+
+train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+sns.catplot(data=train_file, x="Pclass",  kind="count", hue="Survived")
+plt.show()
+
+Wykres wyświetlany przez powyższy kod sugeruje raczej większe prawdopodobieńtwo przeżycia, w zależności od kupionego
+biletu, czy raczej, wybranej klasy podróży. Pasażerowie klasy pierwszej przeżyli w większości podczas gdy większość 
+pasażerów klasy trzeciej zginęła.
+Na wykresie prezentującym zależność między wiekiem, ceną biletu a przeżywalnością widzimy dane sugerujące, że 
+osoby, które przeżyły w klasie niższej to były dzieci, podczas gdy wraz z ceną biletu wiek jednostki, która przeżyła 
+rośnie. Widać to w wykresie prezentowanym przez kod poniżej.
+
+train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+sns.relplot(data=train_file, x="Fare", y="Age", hue="Survived")
+plt.show()
+
+Jak zatem prezentuje się przeżywalność jednostki w zależności od samego wieku?
+
+train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+sns.kdeplot(data=train_file, x="Age", hue="Survived")
+plt.show()
+
+Większość dzieci przeżyła, zatem można śmiało wnioskować, że dzieci miały pierwszeństwo przy ewakuacji. 
+Skoro wiemy już, że priorytetowo ewakuowane były dzieci, to sprawdźmy kto był priorytetyzowany jeśli chodzi o płeć,
+kobiety czy mężczyźni?
+
+train_file.loc[:, "Survived"][train_file["Survived"] == 0] = "No"
+train_file.loc[:, "Survived"][train_file["Survived"] == 1] = "Yes"
+sns.catplot(data=train_file, x="Sex", hue="Survived", kind="count")
+plt.show()
+
+Jak widać na wykresie zdecydowana większość kobiet przeżyła. Było wśród nich nie tylko więcej uratowanych osób, ale też 
+stanowią one ponad połowę uratowanych osób.
+
+Podumowując przeanalizowane dane, można wysunąć następujące wnioski:
+
+1. Ewakuując załogę pierwszeństwo otrzymały dzieci oraz kobiety.
+2. Osoby podróżujące wyższą klasą miały większe szanse na przeżycie. Osoby ocalone podróżujące wyższą klasą były
+w średnim wieku.
 
